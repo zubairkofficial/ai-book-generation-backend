02689f16a12ebf938eda6daf21a745b13bdd39e0
Hamza Ameen <ameenhamza392@gmail.com>
2025-06-02 16:54:06 +0500

    feat:push latest code

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -12,5 +12,5 @@ export default new DataSource({
   database: process.env.DATABASE_NAME,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-  synchronize: false,
+  synchronize: true,
 }); 
